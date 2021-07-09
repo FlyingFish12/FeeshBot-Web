5a099b4913513597fbb16bcb0e834d9291182bcf
Fefe <F33f11f00fum@gmail.com>
2021-07-09 21:25:27 +0100

    v2 les go

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+const colors = require(tailwindcss/colors)
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        navbar: '#131316',
+        navbartext: '#f9f7f3'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
